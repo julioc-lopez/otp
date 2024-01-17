97bc7e560c809ab96d390566cc5e51a7e7a74ee3
z <21096932+julioc-lopez@users.noreply.github.com>
2024-01-16 18:16:37 -0800

    make secret decoding more robust and user friendly

--- auth.go
+++ auth.go
@@ -3,11 +3,9 @@ package otp
 import (
 	"bytes"
 	"crypto/hmac"
-	"encoding/base32"
 	"encoding/binary"
 	"hash"
 	"strconv"
-	"strings"
 	"time"
 )
 
@@ -32,7 +30,7 @@ func GetInterval(period int64) (int64, int64) {
 //
 //	code, err := GetCode("MFRGGZDFMZTWQ2LK", 1, sha1.New, 6)
 func GetCode(secret32 string, iv int64, h Hash, digits int) (string, error) {
-	key, err := base32.StdEncoding.DecodeString(strings.ToUpper(secret32))
+	key, err := decodeBase32(secret32)
 	if err != nil {
 		return "", err
 	}
