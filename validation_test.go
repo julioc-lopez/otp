77caa365657009a89e55de0b14fc38f8702cac3f
z <21096932+julioc-lopez@users.noreply.github.com>
2024-01-16 21:08:31 -0800

    nit: make variable local to the test

--- validation_test.go
+++ validation_test.go
@@ -8,50 +8,50 @@ import (
 	"golang.org/x/crypto/md4" //nolint:staticcheck
 )
 
-var BadKeys = []Key{
-	{
-		Method: "crypto!",
-	},
-	{
-		Method: "totp",
-	},
-	{
-		Method: "totp",
-		Label:  "t@w",
-	},
-	{
-		Method:   "totp",
-		Label:    "t@w",
-		Secret32: "abc123",
-	},
-	{
-		Method:   "totp",
-		Label:    "t@w",
-		Secret32: "MFRGGZDFMZTWQ2LK",
-		Issuer:   "issuer",
-		Algo:     md4.New,
-	},
-	{
-		Method:   "totp",
-		Label:    "t@w",
-		Secret32: "MFRGGZDFMZTWQ2LK",
-		Issuer:   "issuer",
-		Algo:     sha1.New,
-		Digits:   99,
-	},
-	{
-		Method:   "totp",
-		Label:    "t@w",
-		Secret32: "MFRGGZDFMZTWQ2LK",
-		Issuer:   "issuer",
-		Algo:     sha1.New,
-		Digits:   6,
-		Period:   -42,
-	},
-}
-
 func TestBadKeys(t *testing.T) {
-	for _, k := range BadKeys {
+	badKeys := []Key{
+		{
+			Method: "crypto!",
+		},
+		{
+			Method: "totp",
+		},
+		{
+			Method: "totp",
+			Label:  "t@w",
+		},
+		{
+			Method:   "totp",
+			Label:    "t@w",
+			Secret32: "abc123",
+		},
+		{
+			Method:   "totp",
+			Label:    "t@w",
+			Secret32: "MFRGGZDFMZTWQ2LK",
+			Issuer:   "issuer",
+			Algo:     md4.New,
+		},
+		{
+			Method:   "totp",
+			Label:    "t@w",
+			Secret32: "MFRGGZDFMZTWQ2LK",
+			Issuer:   "issuer",
+			Algo:     sha1.New,
+			Digits:   99,
+		},
+		{
+			Method:   "totp",
+			Label:    "t@w",
+			Secret32: "MFRGGZDFMZTWQ2LK",
+			Issuer:   "issuer",
+			Algo:     sha1.New,
+			Digits:   6,
+			Period:   -42,
+		},
+	}
+
+	for _, k := range badKeys {
 		if err := k.Validate(); err == nil {
 			t.Errorf("bad Key didn't produce error on Validate(): %v", k)
 		}
